f16995e0e3c67881381bcb1755f3c54043f86a81
Adam Salter <adam@codebright.net>
2009-09-11 14:58:05 +1000

    - update tests

---
+++ test/bad_encodings/bad3.rb
@@ -0,0 +1,4 @@
+# bad encoding lïne 1
+# encoding: utf-8
+
+# good encoding lïne 4
--- test/bad_encodings_test.rb
+++ test/bad_encodings_test.rb
@@ -3,20 +3,19 @@ require File.dirname(__FILE__) + '/test_helper'
 class BadEncodingsTest < Test::Unit::TestCase
   context 'A file with bad encodings' do
     should 'fail' do
-      bad_file = File.dirname(__FILE__) + '/bad_encodings/bad1.rb'
+      path = File.dirname(__FILE__)
+      bad_file = path + '/bad_encodings/bad1.rb'
       bad_lines = BadEncodings.find_lines_in_file(bad_file)
-      assert_equal bad_lines.class, Array
-      assert_equal bad_lines[0][1], 4
+      assert_equal bad_lines, [[path+"/bad_encodings/bad1.rb", 4]]
     end
   end
 
   context 'A path with bad encodings' do
     should 'fail' do
-      bad_lines = BadEncodings.find_lines_in_path(File.dirname(__FILE__))
-      assert_equal bad_lines.class, Array
-      assert_equal bad_lines[0][1], 1
-      assert_equal bad_lines[1][1], 3
-      assert_equal bad_lines[2][1], 4
+      path = File.dirname(__FILE__)
+      bad_lines = BadEncodings.find_lines_in_path(path)
+      puts bad_lines.inspect
+      assert_equal bad_lines, [[path+"/bad_encodings/bad1.rb", 4], [path+"/bad_encodings/bad2.rb", 1], [path+"/bad_encodings/bad2.rb", 3], [path+"/bad_encodings/bad3.rb", 1]]
     end
   end
 end
